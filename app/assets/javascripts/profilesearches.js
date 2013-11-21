57f86e6ca72783208ec05d1d0321805cde6d0c80
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-11-21 11:17:52 -0800

    Pressing enter for forms, Validation for user-search, Param encoding for Tweet search.

--- app/assets/javascripts/profilesearches.js
+++ app/assets/javascripts/profilesearches.js
@@ -2,9 +2,7 @@ $( document ).ready(function() {
     $( "#profiletweets" ).click(function() {
       $.get('profilesearches/profiletweets.json', function(profiletweets){
         $('.tweet_table').empty();
-        console.log(profiletweets);
         profiletweets.forEach(function(tweet){
-          // console.log(tweet.text);
           $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
         });
       });
@@ -13,11 +11,9 @@ $( document ).ready(function() {
 
 
     $( "#profilehome" ).click(function() {
-      console.log('click');
       $.get('profilesearches/profilehome.json', function(profilehome){
         $('.tweet_table').empty();
         profilehome.forEach(function(tweet){
-          // console.log(tweet.text);
           $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
         });
       });
@@ -28,9 +24,7 @@ $( document ).ready(function() {
 
       $.get('profilesearches/profilementions.json', function(profilementions){
         $('.tweet_table').empty();
-        console.log(profilementions);
         profilementions.forEach(function(tweet){
-          // console.log(tweet.text);
           $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
         });
       });
--- app/assets/javascripts/tweetsearches.js
+++ app/assets/javascripts/tweetsearches.js
@@ -1,10 +1,11 @@
   $( document ).ready(function() {
-    $( "#t_search" ).click(function() {
-      $.get('tweets/search.json?tweetsearch=' + tweetsearch.value, function(profiletweets){
+    $( "#t_search" ).submit(function() {
+      event.preventDefault();
+      var searchString = {tweetsearch: tweetsearch.value};
+      var searchParam = $.param (searchString);
+      $.get('tweets/search.json?' + searchParam, function(profiletweets){
         $('.tweet_table').empty();
-        console.log(profiletweets);
         profiletweets.forEach(function(tweet){
-          console.log(tweet.text);
           $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
         });
       });
