b9cec78c71972ead194efae51ff6046cf4f28275
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-11-21 17:37:39 -0800

    Graph now populates from JSON on Profile/userhome and Usersearch pages.

--- app/assets/javascripts/profilesearches.js
+++ app/assets/javascripts/profilesearches.js
@@ -2,8 +2,8 @@ $( document ).ready(function() {
     $( "#profiletweets" ).click(function() {
       $.get('profilesearches/profiletweets.json', function(profiletweets){
         $('.tweet_table').empty();
-        profiletweets.forEach(function(tweet){
-          $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
+        profiletweets.tweets.forEach(function(tweet){
+          $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")          
         });
       });
     });
@@ -13,9 +13,153 @@ $( document ).ready(function() {
     $( "#profilehome" ).click(function() {
       $.get('profilesearches/profilehome.json', function(profilehome){
         $('.tweet_table').empty();
-        profilehome.forEach(function(tweet){
+        profilehome.tweets.forEach(function(tweet){
           $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
         });
+        var data = profilehome.freqArr;
+
+    //maximum of data you want to use
+    var data_max = 80,
+
+    //number of tickmarks to use
+    num_ticks = 5,
+
+    //margins
+    left_margin = 60,
+    right_margin = 60,
+    top_margin = 30,
+    bottom_margin = 0;
+
+
+    var w = 500,                        //width
+        h = 500,                        //height
+        color = function(id) { return '#00b3dc' };
+
+    var x = d3.scale.linear()
+        .domain([0, data_max])
+        .range([0, w - ( left_margin + right_margin ) ]),
+        y = d3.scale.ordinal()
+        .domain(d3.range(data.length))
+        .rangeBands([bottom_margin, h - top_margin], .5);
+
+
+    var chart_top = h - y.rangeBand()/2 - top_margin;
+    var chart_bottom = bottom_margin + y.rangeBand()/2;
+    var chart_left = left_margin;
+    var chart_right = w - right_margin;
+
+    /*
+     *  Setup the SVG element and position it
+     */
+    var vis = d3.select(".container")
+        .append("svg:svg")
+            .attr("width", w)
+            .attr("height", h)
+        .append("svg:g")
+            .attr("id", "barchart")
+            .attr("class", "barchart")
+
+
+    //Ticks
+    var rules = vis.selectAll("g.rule")
+        .data(x.ticks(num_ticks))
+    .enter()
+        .append("svg:g")
+        .attr("transform", function(d)
+                {
+                return "translate(" + (chart_left + x(d)) + ")";});
+    rules.append("svg:line")
+        .attr("class", "tick")
+        .attr("y1", chart_top)
+        .attr("y2", chart_top + 4)
+        .attr("stroke", "black");
+
+    rules.append("svg:text")
+        .attr("class", "tick_label")
+        .attr("text-anchor", "middle")
+        .attr("y", chart_top)
+        .text(function(d)
+        {
+        return d;
+        });
+    var bbox = vis.selectAll(".tick_label").node().getBBox();
+    vis.selectAll(".tick_label")
+    .attr("transform", function(d)
+            {
+            return "translate(0," + (bbox.height) + ")";
+            });
+
+    var bars = vis.selectAll("g.bar")
+        .data(data)
+    .enter()
+        .append("svg:g")
+            .attr("class", "bar")
+            .attr("transform", function(d, i) { 
+                    return "translate(0, " + y(i) + ")"; });
+
+    bars.append("svg:rect")
+        .attr("x", right_margin)
+        .attr("width", function(d) {
+                return (x(d.value));
+                })
+        .attr("height", y.rangeBand())
+        .attr("fill", color(0))
+        .attr("stroke", color(0));
+
+
+    //Labels
+    var labels = vis.selectAll("g.bar")
+        .append("svg:text")
+            .attr("class", "label")
+            .attr("x", 0)
+            .attr("text-anchor", "right")
+            .text(function(d) {
+                    return d.label;
+                    });
+
+    var bbox = labels.node().getBBox();
+    vis.selectAll(".label")
+        .attr("transform", function(d) {
+                return "translate(0, " + (y.rangeBand()/2 + bbox.height/4) + ")";
+                });
+
+
+    labels = vis.selectAll("g.bar")
+        .append("svg:text")
+        .attr("class", "value")
+        .attr("x", function(d)
+                {
+                return x(d.value) + right_margin + 10;
+                })
+        .attr("text-anchor", "left")
+        .text(function(d)
+        {
+        return "" + d.value + "%";
+        });
+
+    bbox = labels.node().getBBox();
+    vis.selectAll(".value")
+        .attr("transform", function(d)
+        {
+            return "translate(0, " + (y.rangeBand()/2 + bbox.height/4) + ")";
+        });
+
+    //Axes
+    vis.append("svg:line")
+        .attr("class", "axes")
+        .attr("x1", chart_left)
+        .attr("x2", chart_left)
+        .attr("y1", chart_bottom)
+        .attr("y2", chart_top)
+        .attr("stroke", "black");
+     vis.append("svg:line")
+        .attr("class", "axes")
+        .attr("x1", chart_left)
+        .attr("x2", chart_right)
+        .attr("y1", chart_top)
+        .attr("y2", chart_top)
+        .attr("stroke", "black");
+ 
       });
     });
 
@@ -24,7 +168,7 @@ $( document ).ready(function() {
 
       $.get('profilesearches/profilementions.json', function(profilementions){
         $('.tweet_table').empty();
-        profilementions.forEach(function(tweet){
+        profilementions.tweets.forEach(function(tweet){
           $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
         });
       });
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,9 @@
 class Search < ActiveRecord::Base
   def self.analyze(tweets)
+    wordArr = []
+    tempHash = Hash.new(0)
+    tweetHash = {}
+    freqArr = []
     analyzer = Sentimental.new
     tweets.each do |tweet|
       thash = analyzer.get_score tweet.text
@@ -20,7 +24,15 @@ class Search < ActiveRecord::Base
           thash["class"] = "sent-terrible"
         else
       end
+      thash["keywords"].each do |key,value|
+        wordArr.push(key)
+      end
       tweet.attrs[:sentimental] = thash
     end
+    wordArr.each { |word| tempHash[word] += 1 }
+    tempHash.each do |key,value|
+      freqArr << {"label" => key, "value" => value}
+    end
+    tweetHash = {"freqArr" => freqArr, "tweets" => tweets}
   end
 end
