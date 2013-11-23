c2b1fe892e43df3e07afd485593937979a5669b0
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-11-23 15:34:53 -0800

    Tweet search bubble graph nowworks without refreshing.

--- app/assets/javascripts/tweetsearches.js
+++ app/assets/javascripts/tweetsearches.js
@@ -1,3 +1,25 @@
   $( document ).ready(function() {
-
+  var display, key, plot, text;
+  plot = Bubbles();
+  display = function(data) {
+    return plotData("#vis", data, plot);
+  };
+  $("#text-select").val(key);
+  d3.select("#jitter").on("input", function() {
+    return plot.jitter(parseFloat(this.output.value));
   });
+      $( "#t_search" ).submit(function() {
+      event.preventDefault();
+      var searchString = {tweetsearch: tweetsearch.value};
+      var searchParam = $.param (searchString);
+      $.get('tweets/search.json?' + searchParam, function(profiletweets){
+        $('.tweet_table').empty();
+        $('#vis').empty();
+        profiletweets.tweets.forEach(function(tweet){
+          $('.tweet_table').append("<tr class='"+ tweet.sentimental.class + "'><td><a href='http://www.twitter.com/" + tweet.user.screen_name + "'>" + tweet.text + "</a></td></tr>")
+        });
+        display(profiletweets.freqArr);
+        d3.select("#book-title").html(tweetsearch.value);
+      });
+    });
+});
